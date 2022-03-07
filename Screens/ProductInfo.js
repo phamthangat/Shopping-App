7aebb7cd447229ec0e3193aeba02c6d962a8fac6
thang pham <phamthangat397@gmail.com>
2022-03-08 01:36:19 +0700

    show image product

--- Screens/ProductInfo.js
+++ Screens/ProductInfo.js
@@ -1,9 +1,164 @@
-import * as React from 'react';
-import { Text, View, Button } from 'react-native';
-export default function ProductInfo() {
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  Button,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+  Image,
+  Dimensions,
+  Animated,
+} from "react-native";
+import { COLORS } from "../constants";
+import { Items } from "../Database";
+import { Entypo } from "react-native-vector-icons";
+
+export default function ProductInfo({ route, navigation }) {
+  const { productID } = route.params;
+
+  const [product, setProduct] = useState({});
+
+  const { width, height } = Dimensions.get("window");
+
+  const scrollX = new Animated.Value(0);
+  let position = Animated.divide(scrollX, width);
+
+  useEffect(() => {
+    const unsubcribe = navigation.addListener("focus", () => {
+      getDataFromDB();
+    });
+    return unsubcribe;
+  }, [navigation]);
+
+  //   get product data by productID
+  const getDataFromDB = async () => {
+    for (let index = 0; index < Items.length; index++) {
+      if (Items[index].id == productID) {
+        await setProduct(Items[index]);
+        return;
+      }
+    }
+  };
+  console.log(product);
+
+  //product horizontal
+  const renderProduct = ({ item, index }) => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>ProductInfo</Text>
-        </View>
+      <View
+        style={{
+          width: width,
+          height: 240,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image
+          source={item}
+          style={{
+            width: "100%",
+            height: "100%",
+            resizeMode: "contain ",
+          }}
+        ></Image>
+      </View>
     );
+  };
+
+  return (
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: COLORS.white,
+        position: "relative",
+      }}
+    >
+      <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
+      <ScrollView>
+        <View
+          style={{
+            width: "100%",
+            backgroundColor: COLORS.backgroundLight,
+            borderBottomRightRadius: 20,
+            borderBottomLeftRadius: 20,
+            position: "relative",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 4,
+          }}
+        >
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingTop: 20,
+              paddingLeft: 16,
+            }}
+          >
+            <TouchableOpacity>
+              <Entypo
+                name="chevron-left"
+                style={{
+                  fontSize: 18,
+                  color: COLORS.backgroundDark,
+                  padding: 12,
+                  backgroundColor: COLORS.white,
+                  borderRadius: 10,
+                }}
+              />
+            </TouchableOpacity>
+          </View>
+          <FlatList
+            data={product.productImageList ? product.productImageList : null}
+            horizontal
+            renderItem={renderProduct}
+            showsHorizontalScrollIndicator={false}
+            decelerationRate={0.8}
+            snapToInterval={width}
+            bounces={false}
+            onScroll={Animated.event(
+              [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+              { useNativeDriver: false }
+            )}
+          />
+          <View
+            style={{
+              width: "100%",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: 16,
+              marginTop: 32,
+            }}
+          >
+            {product.productImageList
+              ? product.productImageList.map((data, index) => {
+                  let opacity = position.interpolate({
+                    inputRange: [index - 1, index, index + 1],
+                    outputRange: [0.2, 1, 0.2],
+                    extrapolate: "clamp",
+                  });
+                  return (
+                    <Animated.View
+                      key={index}
+                      style={{
+                        width: 10,
+                        height: 10,
+                        backgroundColor: COLORS.black,
+                        opacity,
+                        marginHorizontal: 4,
+                        borderRadius: 100,
+                      }}
+                    ></Animated.View>
+                  );
+                })
+              : null}
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
 }
