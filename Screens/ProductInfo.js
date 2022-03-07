3a81600686342f42a968ee2182b7b2b9b7491db8
thang pham <phamthangat397@gmail.com>
2022-03-08 02:09:08 +0700

    productInfo

--- Screens/ProductInfo.js
+++ Screens/ProductInfo.js
@@ -13,7 +13,7 @@ import {
 } from "react-native";
 import { COLORS } from "../constants";
 import { Items } from "../Database";
-import { Entypo } from "react-native-vector-icons";
+import { Entypo, Ionicons } from "react-native-vector-icons";
 
 export default function ProductInfo({ route, navigation }) {
   const { productID } = route.params;
@@ -49,7 +49,7 @@ export default function ProductInfo({ route, navigation }) {
       <View
         style={{
           width: width,
-          height: 240,
+          height: 180,
           alignItems: "center",
           justifyContent: "center",
         }}
@@ -57,9 +57,10 @@ export default function ProductInfo({ route, navigation }) {
         <Image
           source={item}
           style={{
-            width: "100%",
+            width: "80%",
             height: "100%",
-            resizeMode: "contain ",
+            resizeMode: "contain",
+            marginTop:-15
           }}
         ></Image>
       </View>
@@ -130,8 +131,8 @@ export default function ProductInfo({ route, navigation }) {
               flexDirection: "row",
               alignItems: "center",
               justifyContent: "center",
-              marginBottom: 16,
-              marginTop: 32,
+              marginBottom: 10,
+              marginTop: 10,
             }}
           >
             {product.productImageList
@@ -158,7 +159,187 @@ export default function ProductInfo({ route, navigation }) {
               : null}
           </View>
         </View>
+        <View
+          style={{
+            paddingHorizontal: 16,
+            marginTop: 6,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              marginVertical: 14,
+            }}
+          >
+            <Entypo
+              name="shopping-cart"
+              style={{
+                fontSize: 18,
+                color: COLORS.blue,
+                marginRight: 6,
+              }}
+            />
+            <Text
+              style={{
+                fontSize: 12,
+                color: COLORS.black,
+              }}
+            >
+              Shopping
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              marginVertical: 4,
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 24,
+                fontWeight: "600",
+                letterSpacing: 0.5,
+                marginVertical: 4,
+                color: COLORS.black,
+                maxWidth: "84%",
+              }}
+            >
+              {product.productName}
+            </Text>
+            <Ionicons
+              name="link-outline"
+              style={{
+                fontSize: 24,
+                color: COLORS.blue,
+                backgroundColor: COLORS.blue + 10,
+                padding: 8,
+                borderRadius: 100,
+              }}
+            />
+          </View>
+          <Text
+            style={{
+              fontSize: 12,
+              color: COLORS.black,
+              fontWeight: "400",
+              letterSpacing: 1,
+              opacity: 0.5,
+              lineHeight: 20,
+              maxWidth: "85%",
+              maxHeight: 44,
+              marginBottom: 8,
+            }}
+          >
+            {product.description}
+          </Text>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginVertical: 14,
+              borderBottomColor: COLORS.backgroundLight,
+              borderBottomWidth: 1,
+              paddingBottom: 20,
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                width: "80%",
+                alignItems: "center",
+              }}
+            >
+              <View
+                style={{
+                  color: COLORS.blue,
+                  backgroundColor: COLORS.backgroundLight,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  padding: 12,
+                  borderRadius: 100,
+                  marginRight: 10,
+                }}
+              >
+                <Entypo
+                  name="location-pin"
+                  style={{
+                    fontSize: 16,
+                    color: COLORS.blue,
+                  }}
+                />
+              </View>
+              <Text> Ngõ 82 Duy Tân,{"\n"} Cầu Giấy, Hà Nội</Text>
+            </View>
+            <Entypo
+              name="chevron-right"
+              style={{
+                fontSize: 22,
+                color: COLORS.backgroundDark,
+              }}
+            />
+          </View>
+          <View
+            style={{
+              paddingHorizontal: 16,
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                fontWeight: "500",
+                maxWidth: "85%",
+                color: COLORS.black,
+                marginBottom: 4,
+              }}
+            >
+              &#8377; {product.productPrice}.00
+            </Text>
+            <Text>
+              Tax Rate 2%~ &#8377;{product.productPrice / 20} (&#8377;
+              {product.productPrice + product.productPrice / 20})
+            </Text>
+          </View>
+        </View>
       </ScrollView>
+
+      <View
+        style={{
+          position: "absolute",
+          bottom: 10,
+          height: "8%",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          onPress={() => (product.isAvailable ? addToCart(product.id) : null)}
+          style={{
+            width: "86%",
+            height: "90%",
+            backgroundColor: COLORS.blue,
+            borderRadius: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 12,
+              fontWeight: "500",
+              letterSpacing: 1,
+              color: COLORS.white,
+              textTransform: "uppercase",
+            }}
+          >
+            {product.isAvailable ? "Add to cart" : "Not Avialable"}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
